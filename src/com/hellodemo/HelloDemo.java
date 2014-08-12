65b30d0fb1f3d98fecc4a64c8bc25effdfb6aa97
rajanpatil <rajan.rb@gmail.com>
2014-08-12 20:31:16 +0530

    adding HelloDemo.java

---
+++ src/com/hellodemo/HelloDemo.java
@@ -0,0 +1,11 @@
+package com.hellodemo;
+
+/**
+ * Created by rajan on 12/8/14.
+ */
+public class HelloDemo {
+
+    public static void mian(String[] args){
+        System.out.print("Hello Demo!");
+    }
+}
